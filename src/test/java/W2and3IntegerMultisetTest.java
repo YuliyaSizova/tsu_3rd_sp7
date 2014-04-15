fcc31be339e2888c2c460a945c8e8ca1333526ea
ASUS <ASUS@192.168.81.43>
2014-04-15 13:49:17 +0400

    Реализация мультимножества (со списком, знаю, что иногда может быть очень медленным вариантом)

--- src/test/java/W2and3IntegerMultisetTest.java
+++ src/test/java/W2and3IntegerMultisetTest.java
@@ -1,3 +1,4 @@
+
 /**
  * будет готово чуть позже...
  */
@@ -10,22 +11,21 @@ import java.util.Map;
 import java.util.Random;
 
 public class W2and3IntegerMultisetTest {
+
     private static final int NUM_OF_ELEMENTS = 1024;
     private static final int MAX_QUANTITY = 11;
 
     private static final Map<Integer, Integer> makeRandomNumsWithQuantities() {
         Map<Integer, Integer> r = new HashMap<>();
         Random random = new Random();
-
         while (r.size() < NUM_OF_ELEMENTS) {
             int n = random.nextInt();
             if (!r.containsKey(n)) {
                 int q = 0;
-                while ((q = random.nextInt(MAX_QUANTITY)) != 0); // warning! tricky (may be) code
+                while ((q = random.nextInt(MAX_QUANTITY)) == 0);
                 r.put(n, q);
             }
         }
-
         return r;
     }
 
@@ -33,27 +33,27 @@ public class W2and3IntegerMultisetTest {
         for (Map.Entry<Integer, Integer> e : numsAndQuantities.entrySet()) {
             int n = e.getKey();
             int q = e.getValue();
-
-            while (--q > 0) multiset.add(n);
+            while (--q >= 0) {
+                multiset.add(n);
+            }
         }
     }
 
     private static void testElementsExistense(IntegerMultiset multiset, Map<Integer, Integer> numsAndQuantities) {
-        for (Integer n : numsAndQuantities.keySet())
+        for (Integer n : numsAndQuantities.keySet()) {
             assertTrue(multiset.contains(n));
+        }
     }
 
     private static void testElementsPop(IntegerMultiset multiset, Map<Integer, Integer> numsAndQuantities) {
         for (Map.Entry<Integer, Integer> e : numsAndQuantities.entrySet()) {
             int n = e.getKey();
             int q = e.getValue();
-
-            while (--q > 0) {
+            while (--q >= 0) {
                 assertTrue(multiset.contains(n));
                 boolean elementWasExisted = multiset.pop(n);
                 assertTrue(elementWasExisted);
             }
-
             assertFalse(multiset.contains(n));
         }
     }
@@ -62,7 +62,6 @@ public class W2and3IntegerMultisetTest {
     public void testIntegerMultisetBasicOperations() {
         IntegerMultiset multiset = new IntegerMultiset();
         Map<Integer, Integer> numsAndQuantities = makeRandomNumsWithQuantities();
-
         fillMultisetWithNumsWithQuantities(multiset, numsAndQuantities);
         testElementsExistense(multiset, numsAndQuantities);
         testElementsPop(multiset, numsAndQuantities);
@@ -72,9 +71,7 @@ public class W2and3IntegerMultisetTest {
     public void testIntegerMultisetElementsRemove() {
         IntegerMultiset multiset = new IntegerMultiset();
         Map<Integer, Integer> numsAndQuantities = makeRandomNumsWithQuantities();
-
         fillMultisetWithNumsWithQuantities(multiset, numsAndQuantities);
-
         for (Integer n : numsAndQuantities.keySet()) {
             assertTrue(multiset.contains(n));
             boolean elementWasExisted = multiset.remove(n);
